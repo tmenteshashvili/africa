e18ab9710d9892a29bed5b4fed801c93c4dfc074
Tako Menteshashvili <64552231+tmenteshashvili@users.noreply.github.com>
2022-08-02 15:09:59 +0400

    Initial Commit

---
+++ Africa/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Africa
+//
+//  Created by Tako Menteshashvili on 02.08.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
